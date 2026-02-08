864fb0b28b33f701c2bce70cfd741feeaefb221c
Pranav Kubal <pranav.kubal21@gmail.com>
2026-02-08 11:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (76.60%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.c
@@ -0,0 +1,11 @@
+int gcdOfOddEvenSums(int n) {
+    // int oddSum = 0;
+    // int evenSum = 0;
+
+    // oddSum = n * n;
+    // evenSum = n * (n + 1);
+
+    // gcd(n, n + 1) = 1, so the answer is n
+
+    return n;    
+}
