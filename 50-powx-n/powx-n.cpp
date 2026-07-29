56f65e21469c1e883b90bd0a4e3a09c6623a3179
Pranav Kubal <pranav.kubal21@gmail.com>
2026-07-29 21:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.46%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -5,6 +5,8 @@ public:
         if (n == 0) return 1.0;
         if (x == 0) return 0.0;
         if (x == 1) return 1.0;
+        if (x == -1 && n % 2 == 0) return 1.0;
+        if (x == -1 && n % 2 != 0) return -1.0;
 
         long binForm = n; 
         if (n < 0) {
