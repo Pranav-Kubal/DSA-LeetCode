2f9492597199c1359fc4b71b8091e19ff0a14b87
Pranav Kubal <pranav.kubal21@gmail.com>
2026-07-29 21:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.83%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,8 +3,8 @@ public:
     double myPow(double x, int n) {
 
         if (n == 0) return 1.0;
-
         if (x == 0) return 0.0;
+        if (x == 1) return 1.0;
 
         long binForm = n; 
         if (n < 0) {
