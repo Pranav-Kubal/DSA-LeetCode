f693fb99df76a10ab5e302f59c7bf0af29f51fb5
Pranav Kubal <pranav.kubal21@gmail.com>
2026-07-31 17:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (11.16%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 1; // peak can't be starting element
+        int end = arr.size() - 2; // peak can't be ending element
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]) {
+                return mid;
+            }
+
+            if (arr[mid - 1] < arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;   
+            }
+        }
+
+        return -1;
+    }
+};
